fb8771a1e26d852cb48a87d0fa7d93479e8e7611
parapara1143-hub <parapara1143@gmail.com>
2025-11-02 11:01:12 -0300

    Delete scripts directory

--- scripts/backup_db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-DATE=$(date +%Y%m%d_%H%M%S)
-PGURL=${DATABASE_URL:-"postgresql://sos:sos@localhost:5432/sosdb"}
-pg_dump "$PGURL" > "backup_${DATE}.sql"
-echo "Backup gerado: backup_${DATE}.sql"
